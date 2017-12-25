7c65ccfa20c7cb6871c2e4effb991466cf23b5e6
Eduardo Tavares <etavaresf@gmail.com>
2017-12-25 12:43:25 -0300

    Ajustes de novo

--- DependencyInjection/ArquivoLogger.php
+++ DependencyInjection/ArquivoLogger.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DI;
+namespace DependencyInjection;
 
 class ArquivoLogger implements ILogger {
 
--- DependencyInjection/ConsoleLogger.php
+++ DependencyInjection/ConsoleLogger.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DI;
+namespace DependencyInjection;
 
 class ConsoleLogger implements ILogger {
 
